139309969ad086152105ca4195d0dba48a004cc5
Lonny Snyder <lonnysnyder81@gmail.com>
2017-06-01 19:24:27 -0500

    think i finished question class

--- src/Question.java
+++ src/Question.java
@@ -5,4 +5,16 @@ public class Question {
     protected String question;
     protected String instructions;
 
+
+
+    public void setQuestion(String aQuestion){
+        this.question=aQuestion;
+    }
+    public String getQuestion(Question question){
+        return this.question;
+    }
+    public void setInstructions(String aInstruction){
+        this.instructions=aInstruction;
+    }
+
 }
