e4372f282c877c07d9d6fac19cc61ced2d902a8d
Lonny Snyder <lonnysnyder81@gmail.com>
2017-06-01 19:19:46 -0500

    added Question and Checkbox classes

---
+++ src/CheckBox.java
@@ -0,0 +1,7 @@
+/**
+ * Created by lonny on 6/1/2017.
+ */
+public class CheckBox extends Question{
+    private String instructions="Please enter all numbers that apply without commas.";
+
+}
---
+++ src/Question.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lonny on 6/1/2017.
+ */
+public class Question {
+    protected String question;
+    protected String instructions;
+
+}
